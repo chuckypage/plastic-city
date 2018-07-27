68a41befdb95b5b49d7e5e4502854d2c9b6a8c43
Chuck Page <pagec892@newschool.edu>
2018-07-27 21:37:48 +0000

    message

---
+++ script.js
@@ -0,0 +1,28 @@
+var interval = undefined;
+$(document).ready(function () {
+    interval = setInterval(getNext, 10000000); // milliseconds
+    $('.console-forward').on('click', getNext);
+    $('.console-back').on('click', getPrev);
+});
+
+function getNext() {
+    var $curr = $('.slideshow img:visible'),
+        $next = ($curr.next().length) ? $curr.next() : $('.slideshow img').first();
+
+    transition($curr, $next);
+}
+
+function getPrev() {
+    var $curr = $('.slideshow img:visible'),
+        $next = ($curr.prev().length) ? $curr.prev() : $('.slideshow img').last();
+    transition($curr, $next);
+}
+
+function transition($curr, $next) {
+    clearInterval(interval);
+
+    $next.css('z-index', 2).fadeIn('slow', function () {
+        $curr.hide().css('z-index', 0);
+        $next.css('z-index', 1);
+    });
+}
