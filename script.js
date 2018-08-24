891a6a88fda60a5d80e625d6c5e311c9f17208f6
Chuck Page <pagec892@newschool.edu>
2018-08-24 03:46:14 +0000

    message

--- script.js
+++ script.js
@@ -27,69 +27,138 @@ function transition($curr, $next) {
     });
 }
 
-var currentAudio = 0;
-var audioClasses = [ 'barbiehorse.mp3', 'powerbeauty.mp3', 'groove.m4a', 'beautiful.m4a', 'elevator.m4a', 'perfectbody.mp3'];
-var songs = []
+var tracklist = [
+    {
+    src: 'angels.mp3',
+    cover: 'http://www.tunohastenidoinfancia.com/wp-content/uploads/2012/04/quantum-leap_Final-233x300.jpg'},
+{
+    src: 'http://feedproxy.google.com/~r/droidcast/~5/WYL0gD1VwXQ/56-droidcast-android-galaxy-siii-noticias-correos_md_1209538_1.mp3',
+    cover: 'http://www.droidcast.es/wp-content/uploads/2012/01/top-post-podcast.jpg'},
+{
+    src: 'http://www.arcadiagamers.com/elclubvintage/ECV66_25may.mp3',
+    cover: 'waterfall.gif'},
+{
+    src: 'http://www.kafelog.com/d/kafelog112.mp3',
+    cover: 'http://www.kafelog.com/wp-content/themes/yoko/images/headers/logoS.jpg'}
+];
 
-function prevAudio() {
-	songs[currentAudio].pause();
-	currentAudio -=1;
-	if (currentAudio < 0) currentAudio = audioClasses.length -1;
-	setAudio();
-}
+var player = false;
+var track = 1;
+var lastTrack = -1;
 
-function nextAudio() {
-	songs[currentAudio].pause();
-	currentAudio += 1;
-	if (currentAudio >= audioClasses.length) currentAudio = 0;
-	setAudio();
-}
+var play = function() {
+    if (lastTrack != track) {
+        player.src = tracklist[track - 1].src;
+    }
 
-function setAudio() {
-		songs[currentAudio].play();
+    lastTrack = track;
 
+    if (player.paused) {
+        player.play();
+    }
+};
 
-}
+$(document).ready(function() {
+    player = document.createElement('audio');
 
-// generating audio elements for all songs
-for (var i =0; i < audioClasses.length; ++i) {
-	var currentSong = audioClasses[i]
-	var audio = new Audio();
-	audio.src= currentSong
-	songs.push(audio)
-}
+    $('a.player-control').click(function(event) {
+        event.preventDefault();
+    });
 
-setAudio();
+    $('#play').click(function() {
+        play();
+    });
 
-$('.console-playback').click(function() {
-	handlemessage({
-	  sender: 'suhhh',
-	  type: 'raw',
-	  value: 8
-	});
+    $('#pause').click(function() {
+        if (!player.paused) {
+            player.pause();
+        }
+    });
+
+    $('#next').click(function() {
+        track++;
+
+        if (track > tracklist.length) {
+            track = 1;
+        }
+
+        play();
+    });
+
+    $('#prev').click(function() {
+        track--;
+
+        if (track < 1) {
+            track = tracklist.length;
+        }
+
+        play();
+    });
 });
-$('.console-playforward').click(function() {
-	handlemessage({
-	  sender: 'suhhh',
-	  type: 'raw',
-	  value: 10
-	});
-});
 
-function handlemessage(data) {
+// var currentAudio = 0;
+// var audioClasses = [ 'barbiehorse.mp3', 'powerbeauty.mp3', 'groove.m4a', 'beautiful.m4a', 'elevator.m4a', 'perfectbody.mp3'];
+// var songs = []
 
-	if (data.value == 0) {
-		prevBg();
+// function prevAudio() {
+// 	songs[currentAudio].pause();
+// 	currentAudio -=1;
+// 	if (currentAudio < 0) currentAudio = audioClasses.length -1;
+// 	setAudio();
+// }
 
-	} else if (data.value == 8) {
-		prevAudio();
-	} else if (data.value == 9) {
-		prevAudio();
-	} else if (data.value == 10) {
-		nextAudio();
-	} else if (data.value == 11) {
-		nextAudio();
-	} else if (data.value == 12) {
-		nextAudio();
-	}
-}
+// function nextAudio() {
+// 	songs[currentAudio].pause();
+// 	currentAudio += 1;
+// 	if (currentAudio >= audioClasses.length) currentAudio = 0;
+// 	setAudio();
+// }
+
+// function setAudio() {
+// 		songs[currentAudio].play();
+
+
+// }
+
+// // generating audio elements for all songs
+// for (var i =0; i < audioClasses.length; ++i) {
+// 	var currentSong = audioClasses[i]
+// 	var audio = new Audio();
+// 	audio.src= currentSong
+// 	songs.push(audio)
+// }
+
+// setAudio();
+
+// $('.console-playback').click(function() {
+// 	handlemessage({
+// 	  sender: 'suhhh',
+// 	  type: 'raw',
+// 	  value: 8
+// 	});
+// });
+// $('.console-playforward').click(function() {
+// 	handlemessage({
+// 	  sender: 'suhhh',
+// 	  type: 'raw',
+// 	  value: 10
+// 	});
+// });
+
+// function handlemessage(data) {
+
+// 	if (data.value == 0) {
+// 		prevBg();
+
+// 	} else if (data.value == 8) {
+// 		prevAudio();
+// 	} else if (data.value == 9) {
+// 		prevAudio();
+// 	} else if (data.value == 10) {
+// 		nextAudio();
+// 	} else if (data.value == 11) {
+// 		nextAudio();
+// 	} else if (data.value == 12) {
+// 		nextAudio();
+// 	}
+// }
